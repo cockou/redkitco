fd8825194039f79202ec4a74facacc6b70e393f0
cockou <roluma.7@gmail.com>
2025-07-27 14:26:37 -0600

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
-    preview: {
-      host: '0.0.0.0',
-      port: 4321,
-      allowedHosts: ['redkit.co', 'localhost', '127.0.0.1']
-    }
+    plugins: [tailwindcss()]
   }
 });
